e5f5356773a076ba04f91b47ceb6988530121e5a
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 21:11:09 +0500

    cod

--- app/(web)/courses/page.tsx
+++ app/(web)/courses/page.tsx
@@ -6,13 +6,15 @@ const Page = () => {
     const courseIds = ["1", "2", "3", "4"]
 
     return (
-        <main>
+        <main className='w-full flex flex-col justify-start items-start gap-8'>
             <div>Courses</div>
-            {courseIds?.map((id: any, i: number) => (
-                <Link key={i} href={`/courses/${id}`}
-                    className='px-8 py-4 bg-blue-300 text-blue-800 decoration-blue-800 hover:underline'
-                >Course {id}</Link>
-            ))}
+            <div>
+                {courseIds?.map((id: any, i: number) => (
+                    <Link key={i} href={`/courses/${id}`}
+                        className='px-8 py-4 bg-blue-300 text-blue-800 decoration-blue-800 hover:underline'
+                    >Course {id}</Link>
+                ))}
+            </div>
         </main>
     )
 }
