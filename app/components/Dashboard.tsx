5642a5262eee0f7f18c71805b1b968a532322d2d
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 19:21:49 +0500

    cod

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { getDashboardRouting } from '../utils/functions'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { LogoutButton } from './Header'
 
 const Dashboard = () => {
     const current_path = usePathname()
@@ -14,7 +15,7 @@ const Dashboard = () => {
     }, [])
 
     return (
-        <div className='w-full flex justify-start items-start py-4 px-8 gap-x-8 gap-y-4 flex-wrap bg-red-200'>
+        <div className='w-full flex justify-start items-start py-4 px-8 gap-x-8 gap-y-4 flex-wrap bg-red-200 mb-4'>
             {dashboardRoutes?.map((route: any, i: number) => (
                 <Link href={route?.path} key={i}
                     className={`text-red-800 hover:underline decoration-red-800 ${route?.path === current_path ? "underline" : null}`}
@@ -22,6 +23,7 @@ const Dashboard = () => {
                     {route?.label}
                 </Link>
             ))}
+            <LogoutButton />
         </div>
     )
 }
