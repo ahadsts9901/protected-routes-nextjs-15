46f62c09f71d0a08ba95e72e7a88e5748290844e
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 21:08:27 +0500

    cod

--- app/components/GoBack.tsx
+++ app/components/GoBack.tsx
@@ -1,10 +1,19 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const GoBack = () => {
-  return (
-    <div className=''>GoBack</div>
-  )
+    const [back, set_back] = useState(false)
+
+    useEffect(() => {
+        if (!back) return
+        window.history.back()
+    }, [back])
+
+    return (
+        <button onClick={() => set_back(true)}
+            className='cursor-pointer capitalize bg-slate-200 px-4 py-2 w-[100px] text-left mb-4'
+        >Go Back</button>
+    )
 }
 
 export default GoBack
