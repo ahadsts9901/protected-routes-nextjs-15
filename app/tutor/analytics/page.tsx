d4b14c0f76ede837efe76d14cb96d4028e7c7ebd
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 17:53:47 +0500

    cod

---
+++ app/tutor/analytics/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Tutor Analytics</div>
+  )
+}
+
+export default Page
---
+++ app/tutor/students/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Tutor Students</div>
+  )
+}
+
+export default Page
