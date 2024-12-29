0d7a6c7b5cc4132e24b19751e096edaad9ba9666
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 19:53:15 +0500

    routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,48 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const adminRoutes = [
+    "/admin/analytics",
+    "/admin/courses",
+    "/admin/sub-admins",
+    "/admin/tutors",
+    "/admin/students",
+]
+
+const subAdminRoutes = [
+    "/sub-admin/analytics",
+    "/sub-admin/courses",
+    "/sub-admin/tutors",
+    "/sub-admin/students",
+]
+
+const tutorRoutes = [
+    "/tutor/analytics",
+    "/tutor/courses",
+    "/tutor/students",
+]
+
+const studentRoutes = [
+    "/student/analytics",
+    "/student/courses",
+]
+
+const publicRoutes = [
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+
 export const middleware = async (req: NextRequest, res: NextResponse) => {
-    // NextResponse.redirect(new URL("/contact"))
+    try {
+        // NextResponse.redirect(new URL("/contact"))
+        const role = req.cookies.get("role")?.value
+
+    } catch (error) {
+        console.error(error)
+        NextResponse.json({
+            message: "middleware server error"
+        }, { status: 500 })
+    }
 }
