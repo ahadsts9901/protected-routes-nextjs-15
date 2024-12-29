c0bd6eb1c934fac10b12ec66c10cc4c7877a4cd5
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 17:51:45 +0500

    cod

---
+++ app/admin/analytics/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Admin Analytics</div>
+  )
+}
+
+export default Page
---
+++ app/admin/sub-admins/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Admin SubAdmins</div>
+  )
+}
+
+export default Page
