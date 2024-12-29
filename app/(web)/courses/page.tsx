8c6a374cedc454412531b6d5cc7aa800ff826fa0
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 20:56:36 +0500

    dunamic

--- app/(web)/courses/page.tsx
+++ app/(web)/courses/page.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
+
+    const courseIds = ["1", "2", "3", "4"]
+
     return (
-        <div>Courses</div>
+        <main>
+            <div>Courses</div>
+            {courseIds?.map((id: any, i: number) => (
+                <Link key={i} href={`/courses/${id}`}
+                    className='px-8 py-4 bg-blue-300 text-blue-800 decoration-blue-800 hover:underline'
+                >Course {id}</Link>
+            ))}
+        </main>
     )
 }
 
