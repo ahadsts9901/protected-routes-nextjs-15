180dc286e99ceb3300356c608e9648496756f9d3
ahadsts9901 <ahadsts990@gmail.com>
2024-12-29 19:25:22 +0500

    cod

---
+++ app/middleware.ts
@@ -0,0 +1,5 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = async (req: NextRequest, res: NextResponse) => {
+    NextResponse.redirect(new URL("/contact"))
+}
